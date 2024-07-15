888fb8d72f104b0403f48a7d8ee4786c398235a8
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-16 02:18:17 +0530

    created the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import ProductsCard from "../components/ProductsCard";
 
 const Home = () => {
+  const addToHandler = () => {};
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <section></section>
 
-export default Home
+      <h1>
+        Latest Product
+        <Link to={"/search"} className="findmore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        <ProductsCard
+          productId="asd"
+          name="MacBook"
+          price={2334}
+          stock={435}
+          handler={addToHandler}
+          photo="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mba13-midnight-select-202402?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1708367688034"
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
