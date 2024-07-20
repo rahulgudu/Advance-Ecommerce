d03aa6aa788e57074ec111f7cea8df6d5db01590
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-20 13:15:08 +0530

    added the search page

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import { useState } from "react";
+import ProductsCard from "../components/ProductsCard";
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState(100000);
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
 
-export default Search
+  const addToCartHandler = () => {};
+
+  const isPrevPage = false;
+  const isNextPage = true;
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price (Low to High)</option>
+            <option value="desc">Price (High to Low)</option>
+          </select>
+        </div>
+
+        <div>
+          <h4>Max Price: {maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={10000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="">Sample 1</option>
+            <option value="">Sample 2</option>
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div className="search-product-list">
+          <ProductsCard
+            productId="asdfs"
+            name="Mackbook"
+            price={4545}
+            stock={435}
+            handler={addToCartHandler}
+            photo="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mba13-midnight-select-202402?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1708367688034"
+          />
+        </div>
+
+        <article>
+          <button disabled={!isPrevPage} onClick={() => setPage((prev) => prev - 1)}>Prev</button>
+          <span>
+            {page} of {4}
+          </span>
+          <button disabled={!isNextPage} onClick={() => setPage((prev) => prev + 1)}>Next</button>
+        </article>
+      </main>
+    </div>
+  );
+};
+
+export default Search;
