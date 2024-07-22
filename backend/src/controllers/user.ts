868d1a783b7ff11d4d298ed8280c201fe350392c
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-23 02:13:58 +0530

    created custom error classes

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,8 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user.js";
 import { NewUserRequestBody } from "../types/types.js";
-export const newUser = async (req: Request, res: Response) => {
-    try {
+import ErrorHandler from "../utils/utility-class.js";
+import { TryCatch } from "../middlewares/error.js";
+export const newUser = TryCatch(
+    async (req: Request<{}, {}, NewUserRequestBody>, res: Response, next: NextFunction) => {
         const { name, email, photo, gender, _id, dob } = req.body;
 
         const user = await User.create({
@@ -17,10 +19,5 @@ export const newUser = async (req: Request, res: Response) => {
             success: true,
             message: `Welcome, ${user.name}`
         })
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: "Internal Server Error"
-        })
     }
-}
+)
