3de28b9d3a3f4eaa6f3475419c523787d5a16dc2
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-24 10:42:25 +0530

    added the get all users, get user and delete user endpoint

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -6,18 +6,64 @@ import { TryCatch } from "../middlewares/error.js";
 export const newUser = TryCatch(
     async (req: Request<{}, {}, NewUserRequestBody>, res: Response, next: NextFunction) => {
         const { name, email, photo, gender, _id, dob } = req.body;
+        // login also added here
+        let user = await User.findById(_id);
+        if (user) {
+            res.status(200).json({
+                success: true,
+                message: `Welcome, ${user.name}`
+            });
+        }
 
-        const user = await User.create({
+        if (!name || !email || !photo || !gender || !_id || dob) {
+            return next(new ErrorHandler("Please ill all details", 400));
+        }
+
+        user = await User.create({
             name,
             email,
             photo,
             gender,
             _id,
             dob: new Date(dob)
-        })
+        });
         res.status(201).json({
             success: true,
             message: `Welcome, ${user.name}`
-        })
+        });
     }
-)
+);
+
+export const getAllUser = TryCatch(async (req, res, next) => {
+    const users = await User.find({});
+    return res.status(200).json({
+        success: true,
+        users
+    })
+});
+
+
+export const getUser = TryCatch(async (req, res, next) => {
+    const id = req.params.id;
+    const user = await User.findById(id);
+
+    if (!user) return next(new ErrorHandler("Invalid Id", 400));
+
+
+    return res.status(200).json({
+        success: true,
+        user
+    });
+});
+
+export const deleteUser = TryCatch(async (req, res, next) => {
+    const id = req.params.id;
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) return next(new ErrorHandler("Invalid Id", 400));
+
+    return res.status(200).json({
+        success: true,
+        message: "User deleted successfully"
+    });
+});
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { newUser } from "../controllers/user.js";
+import { deleteUser, getAllUser, getUser, newUser } from "../controllers/user.js";
 
 const app = express.Router();
 
 app.post("/new", newUser);
+app.get("/all", getAllUser);
+
+// chaning
+app.route("/:id").get(getUser).delete(deleteUser);
 
 export default app;
